ed8be34614e16ac9751b475c7969ba73fef54fb6
R267 <74021908+R267@users.noreply.github.com>
2026-02-09 22:09:48 +0000

    Add files via upload

---
+++ kw_library/models/author.py
@@ -0,0 +1,16 @@
+import logging
+
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class Author(models.Model):
+    _name = 'kw.lib.author'
+    _description = 'Author'
+
+    name = fields.Char()
+
+    active = fields.Boolean(
+        default=True,)
+    isbn = fields.Char()
